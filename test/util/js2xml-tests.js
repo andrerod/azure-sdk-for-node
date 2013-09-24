a5250d7cb4a0ea35960624e9904a2b281235c87f
Andre Rodrigues <andrerod@microsoft.com>
2013-09-24 15:40:44 +0100

    More

--- test/util/js2xml-tests.js
+++ test/util/js2xml-tests.js
@@ -62,12 +62,74 @@ describe('js2xml', function() {
       var element1 = js2xml.createElement(name1, namespace1);
       var element2 = js2xml.createElement(name2, namespace2);
 
-      element1.children.length.should.equal(0);
+      should.not.exist(element1.value);
 
       js2xml.addChildElement(element1, element2);
 
-      element1.children.length.should.equal(1);
-      element1.children[0].name.should.equal(name2);
+      element1.value.length.should.equal(1);
+      element1.value[0].name.should.equal(name2);
+
+      done();
+    });
+  });
+
+  describe('serializeDocument', function () {
+    it('should work with a child', function (done) {
+      var name1 = 'name1';
+      var name2 = 'name2';
+      var namespace1 = 'namespace1';
+      var namespace2 = 'namespace2';
+      var value2 = 3;
+
+      var element1 = js2xml.createElement(name1, namespace1);
+      var element2 = js2xml.createElement(name2, namespace2);
+      js2xml.setElementValue(element2, value2);
+      js2xml.addChildElement(element1, element2);
+
+      var result = js2xml.serializeDocument(element1);
+
+      result.should.equal('<?xml version="1.0" encoding="utf-8" standalone="yes"?><name1><name2>' + value2 + '</name2></name1>');
+
+      done();
+    });
+
+    it('should work with a child and attriutes', function (done) {
+      var name1 = 'name1';
+      var name2 = 'name2';
+      var namespace1 = 'namespace1';
+      var namespace2 = 'namespace2';
+      var value2 = 3;
+      var attrName1 = 'attrName1';
+      var attrName2a = 'attrName2a';
+      var attrName2b = 'attrName2b';
+      var attrValue1 = 'attrValue1';
+      var attrValue2a = 'attrValue2a';
+      var attrValue2b = 'attrValue2b';
+
+      var element1 = js2xml.createElement(name1, namespace1);
+      var attr1 = js2xml.createAttribute(attrName1);
+      js2xml.setAttributeValue(attr1, attrValue1);
+      js2xml.addAttribute(element1, attr1);
+
+      var element2 = js2xml.createElement(name2, namespace2);
+      js2xml.setElementValue(element2, value2);
+
+      var attr2a = js2xml.createAttribute(attrName2a);
+      js2xml.setAttributeValue(attr2a, attrValue2a);
+      js2xml.addAttribute(element2, attr2a);
+
+      var attr2b = js2xml.createAttribute(attrName2b);
+      js2xml.setAttributeValue(attr2b, attrValue2b);
+      js2xml.addAttribute(element2, attr2b);
+
+      js2xml.addChildElement(element1, element2);
+
+      var result = js2xml.serializeDocument(element1);
+
+      result.should.equal('<?xml version="1.0" encoding="utf-8" standalone="yes"?><name1 ' +
+        attrName1 + '="' + attrValue1 + '">' +
+        '<name2 ' + attrName2a + '="' + attrValue2a + '" ' + attrName2b + '="' + attrValue2b + '">' +
+        value2 + '</name2></name1>');
 
       done();
     });
